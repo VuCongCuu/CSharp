c114f1fb2462ff80323154617b25484825fd6e98
VuCongCuu <congcuu1102@gmail.com>
2022-03-09 16:37:10 +0700

    c

---
+++ PtBac2/Function.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PtBac2
+{
+    internal class Function
+    {
+        public static void nhap(out double a, out double b, out double c)
+        {
+            Console.WriteLine("Nhập a:");
+            a= Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Nhập b:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Nhập c:");
+            c = Convert.ToDouble(Console.ReadLine());
+        }
+        public static int HienThi(double a, double b, double c, ref double x1, ref double x2)
+        {
+            
+            double detar = b * b - 4 * a * c;
+            if( detar < 0)          
+                return  0;
+              
+            else if  ( detar == 0)
+            {
+                 x1 = x2= -b / (2 * a);
+                return  1;
+            }
+            else
+            {
+                x1 = (-b - Math.Sqrt(detar)) / (2 * a);
+                x2 =  (-b + Math.Sqrt(detar)) / (2 * a);
+                return 2;
+
+            }
+          
+        }
+    }
+}
---
+++ PtBac2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PtBac2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            double a,b,c,x1,x2;
+            
+            x1 = 0; x2 = 0;
+            Function.nhap(out a, out b, out c);
+            int i = Function.HienThi(a,b,c,ref x1,ref x2);
+            if(i == 0)
+            {
+                Console.WriteLine("Phương trinh vô nghiệm");
+            } else if(i == 1)
+            {
+                Console.WriteLine("Phương trinh có 1 nghiêm x1=x2={0}",x1);
+            }
+            else
+            {
+                Console.WriteLine("Phương trình có 2 nghiệm x1 = {0}, x2 = {1}",x1,x2);
+            }
+        }
+    }
+}
